40629bd59db65c3806ed9939866e4a6be16227f8
elearninfotech1 <91933957+elearninfotech1@users.noreply.github.com>
2024-03-05 08:21:42 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,2 +1,2 @@
 let mongoose = require("mongoose");
-mongoose.connect("mongodb+srv://elearninfotech1:OaBUpDMQtNdl50XO@cluster0.heuewkc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/lpdb");
+mongoose.connect("mongodb+srv://elearninfotech1:OaBUpDMQtNdl50XO@cluster0.heuewkc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
